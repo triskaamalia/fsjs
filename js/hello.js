713dfce6a9fab652ba4d34cad7bb37731dca2d85
triskaamalia <triskaamalia97@gmai.com>
2022-06-14 16:24:12 +0700

    first commit

---
+++ js/hello.js
@@ -0,0 +1,11 @@
+for (let i = 5; i >= 0; i--) {
+    console.log('*'.repeat(i))
+}
+
+let x = 10;
+  x %= 5;
+  console.log(x);
+
+let text = 'Sedang belajar ';
+ text += 'Java Script';
+  console.log(text);
